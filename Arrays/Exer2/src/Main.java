5840d7add2c4bc65cedaac66b4d16a5432780869
João Pires <joaoramospires>
2026-02-20 19:02:37 +0000

    Pegar e calcular a média dos preços dos produtos

--- Arrays/Exer2/src/Main.java
+++ Arrays/Exer2/src/Main.java
@@ -7,8 +7,32 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Introduza a quantidade de produtos: ");
+        int n = sc.nextInt();
+
+        Entidades [] produtos = new Entidades[n];
+
+        for(int i = 0; i < n ; i++){
+
+            System.out.println("Introduza o seu do produto: ");
+            String nome = sc.next();
+
+            System.out.println("Introduza o preço do produto: ");
+            double preco = sc.nextDouble();
+
+            produtos[i] = new Entidades(nome, preco);
+        }
+
+        double soma = 0.0;
+        for(int i = 0; i < n ; i++){
+            //Faço a soma dos preços de todos os produtos
+            soma += produtos[i].getPreco();
+        }
 
 
+    System.out.println(String.format("A média dos preços é %.2f", soma/n));
 
+
+    sc.close();
     }
 }
